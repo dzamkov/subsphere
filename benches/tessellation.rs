0cdcb9c10f456ba0649ad9c941d0538cf4599bc9
Morgan Bennet <98787614+MorganBennetDev@users.noreply.github.com>
2025-05-20 17:16:57 -0600

    Switch benchmarking to criterion for better analysis

--- benches/tessellation.rs
+++ benches/tessellation.rs
@@ -1,10 +1,46 @@
 use std::hint::black_box;
 use std::num::NonZero;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use subsphere::prelude::*;
 
-fn main() {
-    divan::main();
-}
+criterion_group!(
+    trisphere_octa,
+    trisphere_octa_fuller_b_0_face,
+    trisphere_octa_fuller_b_0_vertex,
+    trisphere_octa_gnomonic_b_0_face,
+    trisphere_octa_gnomonic_b_0_vertex
+);
+
+criterion_group!(
+    trisphere_icosa,
+    trisphere_icosa_fuller_b_0_face,
+    trisphere_icosa_fuller_b_0_vertex,
+    trisphere_icosa_gnomonic_b_0_face,
+    trisphere_icosa_gnomonic_b_0_vertex,
+);
+
+criterion_group!(
+    hexsphere_octa,
+    hexsphere_octa_fuller_b_bmod3_face,
+    hexsphere_octa_fuller_b_bmod3_vertex,
+    hexsphere_octa_gnomonic_b_bmod3_face,
+    hexsphere_octa_gnomonic_b_bmod3_vertex
+);
+
+criterion_group!(
+    hexsphere_icosa,
+    hexsphere_icosa_fuller_b_bmod3_face,
+    hexsphere_icosa_fuller_b_bmod3_vertex,
+    hexsphere_icosa_gnomonic_b_bmod3_face,
+    hexsphere_icosa_gnomonic_b_bmod3_vertex,
+);
+
+criterion_main!(
+    trisphere_octa,
+    trisphere_icosa,
+    hexsphere_icosa,
+    hexsphere_octa
+);
 
 fn tessellation_vertex_benchmark<Tessellation: subsphere::Sphere>(sphere: Tessellation) {
     black_box(
@@ -22,226 +58,306 @@ fn tessellation_face_benchmark<Tessellation: subsphere::Sphere>(sphere: Tessella
     black_box(indices);
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_icosa_fuller_b_0_vertex(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Icosa,
-        subsphere::proj::Fuller,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_vertex_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_icosa_gnomonic_b_0_vertex(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Icosa,
-        subsphere::proj::Gnomonic,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_vertex_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_octa_fuller_b_0_vertex(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Octa,
-        subsphere::proj::Fuller,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_vertex_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_octa_gnomonic_b_0_vertex(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Octa,
-        subsphere::proj::Gnomonic,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_vertex_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_icosa_fuller_b_0_face(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Icosa,
-        subsphere::proj::Fuller,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_face_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_icosa_gnomonic_b_0_face(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Icosa,
-        subsphere::proj::Gnomonic,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_face_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_octa_fuller_b_0_face(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Octa,
-        subsphere::proj::Fuller,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_face_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn trisphere_octa_gnomonic_b_0_face(b: u32) {
-    let sphere = subsphere::TriSphere::new(
-        subsphere::BaseTriSphere::Octa,
-        subsphere::proj::Gnomonic,
-        NonZero::new(b).unwrap(),
-        0,
-    );
-    
-    tessellation_face_benchmark(sphere);
-}
-
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_icosa_fuller_b_bmod3_vertex(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_icosa_fuller_b_0_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_icosa_fuller_b_0_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Icosa,
             subsphere::proj::Fuller,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_vertex_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_icosa_gnomonic_b_bmod3_vertex(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_icosa_gnomonic_b_0_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_icosa_gnomonic_b_0_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Icosa,
             subsphere::proj::Gnomonic,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_vertex_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_octa_fuller_b_bmod3_vertex(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_octa_fuller_b_0_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_octa_fuller_b_0_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Octa,
             subsphere::proj::Fuller,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_vertex_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_octa_gnomonic_b_bmod3_vertex(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_octa_gnomonic_b_0_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_octa_gnomonic_b_0_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Octa,
             subsphere::proj::Gnomonic,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_vertex_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_icosa_fuller_b_bmod3_face(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_icosa_fuller_b_0_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_icosa_fuller_b_0_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Icosa,
             subsphere::proj::Fuller,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_face_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_icosa_gnomonic_b_bmod3_face(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_icosa_gnomonic_b_0_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_icosa_gnomonic_b_0_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Icosa,
             subsphere::proj::Gnomonic,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_face_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_octa_fuller_b_bmod3_face(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_octa_fuller_b_0_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_octa_fuller_b_0_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Octa,
             subsphere::proj::Fuller,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_face_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
 
-#[divan::bench(args = [1, 2, 4, 8, 16, 32, 64, 128])]
-fn hexsphere_octa_gnomonic_b_bmod3_face(b: u32) {
-    let c = b % 3;
-    
-    let sphere = subsphere::HexSphere::from_kis(
-        subsphere::TriSphere::new(
+fn trisphere_octa_gnomonic_b_0_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("trisphere_octa_gnomonic_b_0_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let sphere = subsphere::TriSphere::new(
             subsphere::BaseTriSphere::Octa,
             subsphere::proj::Gnomonic,
             NonZero::new(b).unwrap(),
-            c,
-        )
-    ).unwrap();
-    
-    tessellation_face_benchmark(sphere);
+            0,
+        );
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_icosa_fuller_b_bmod3_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_icosa_fuller_b_bmod3_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Icosa,
+                subsphere::proj::Fuller,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_icosa_gnomonic_b_bmod3_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_icosa_gnomonic_b_bmod3_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Icosa,
+                subsphere::proj::Gnomonic,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_octa_fuller_b_bmod3_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_octa_fuller_b_bmod3_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Octa,
+                subsphere::proj::Fuller,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_octa_gnomonic_b_bmod3_vertex(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_octa_gnomonic_b_bmod3_vertex");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Octa,
+                subsphere::proj::Gnomonic,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_icosa_fuller_b_bmod3_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_icosa_fuller_b_bmod3_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Icosa,
+                subsphere::proj::Fuller,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_icosa_gnomonic_b_bmod3_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_icosa_gnomonic_b_bmod3_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Icosa,
+                subsphere::proj::Gnomonic,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_octa_fuller_b_bmod3_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_octa_fuller_b_bmod3_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Octa,
+                subsphere::proj::Fuller,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
+}
+
+fn hexsphere_octa_gnomonic_b_bmod3_face(c: &mut Criterion) {
+    let mut group = c.benchmark_group("hexsphere_octa_gnomonic_b_bmod3_face");
+    for b in [1, 2, 4, 8, 16, 32, 64, 128] {
+        let c = b % 3;
+        
+        let sphere = subsphere::HexSphere::from_kis(
+            subsphere::TriSphere::new(
+                subsphere::BaseTriSphere::Octa,
+                subsphere::proj::Gnomonic,
+                NonZero::new(b).unwrap(),
+                c,
+            )
+        ).unwrap();
+        
+        group.bench_with_input(BenchmarkId::from_parameter(b), &b, |bencher, _| {
+            bencher.iter(|| tessellation_vertex_benchmark(sphere));
+        });
+    }
+    group.finish();
 }
